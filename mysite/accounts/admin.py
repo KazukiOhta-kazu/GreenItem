ecedbe62e0a22e85d1943cd10fdbb36c8f46d09f
Kazuki Ohta <kazu.maron.0707@icloud.com>
2023-01-24 09:11:26 +0900

    ユーザーの管理ページの作成

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "is_staff", "is_superuser", "is_active",)
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
