fe055d92114e8f89156d0052a028434016074175
Kazuki Ohta <kazu.maron.0707@icloud.com>
2023-01-26 12:52:23 +0900

    サインアップのテストを作成

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('account_signup')
+        data = {
+            'email': 'example@example.com',
+            'password1': 'Pass0101',
+            'password2': 'Pass0101'
+        }
+        self.response = self.client.post(url, data)
+
+    def test_user_creation(self):
+        '''
+        ユーザーが作成できているか
+        '''
+        self.assertIs(User.objects.exists(), True)
+
+    def test_redirection(self):
+        '''
+        ユーザーを作成すると、メールアドレスの確認ページにリダイレクトされているか
+        '''
+        redirect_url = reverse('account_email_verification_sent')
+        self.assertEqual(self.response.status_code, 302)
+        self.assertRedirects(self.response, redirect_url)
